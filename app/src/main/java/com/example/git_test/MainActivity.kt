5088ceeec52287e1f627f7d0bc411f0804f81c56
KazanskiiPN <fokkoo@yandex.ru>
2022-11-16 18:17:43 +0300

    23  DAO is working  edeting data from editText

--- app/src/main/java/com/example/git_test/MainActivity.kt
+++ app/src/main/java/com/example/git_test/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.git_test.databinding.ActivityMainBinding
 import com.example.git_test.model.DetailFragment
 import com.example.git_test.model.DetailViewModel
 
@@ -12,18 +13,18 @@ import com.example.git_test.model.DetailViewModel
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: DetailViewModel
-    //  private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //   binding = ActivityMainBinding.inflate(layoutInflater)
-        //   setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
 
         readSettings()
         initeMyView()
 
-        //   binding.textView.text = "binding text"
+        //  binding.textView.text = "binding text"
         //    binding.button3.text = "binding text"
 
         //    FrameLayout layout = new FrameLayout(this);
@@ -43,14 +44,27 @@ class MainActivity : AppCompatActivity() {
     //R.id.button_recycl_view
     //binding.buttonRecyclView.id
     private fun initeMyView() {
+
+
+        binding.ButtonHistory.setOnClickListener() {
+            addFragment(HistoryFragment())
+
+            val toast = Toast.makeText(
+                    applicationContext,
+                    "Кнопка Button_History вызвана", Toast.LENGTH_SHORT
+            )
+            toast.show()
+
+        }
+
+
+
         findViewById<View>(R.id.button_recycl_view).setOnClickListener {
-        //    addFragment(Fragment1firstRW())
+            //    addFragment(Fragment1firstRW())
 
             val weather: Weather = Weather(
                     getDefaultCity(), 0, 0
             )
-
-
 
 
             val toast = Toast.makeText(
@@ -60,7 +74,9 @@ class MainActivity : AppCompatActivity() {
             toast.show()
         }
         findViewById<View>(R.id.button_fragment2).setOnClickListener {
-            addFragment(HistoryFragment())
+
+
+            // addFragment(HistoryFragment())
 
             val toast = Toast.makeText(
                     applicationContext,
@@ -68,15 +84,11 @@ class MainActivity : AppCompatActivity() {
             )
             toast.show()
         }
-        findViewById<View>(R.id.Button_History).setOnClickListener {
-            addFragment(HistoryFragment())
 
-            val toast = Toast.makeText(
-                    applicationContext,
-                    "Кнопка Button_History вызвана", Toast.LENGTH_SHORT
-            )
-            toast.show()
-        }
+        /*  findViewById<View>(R.id.Button_History).setOnClickListener {
+
+          }
+          */
         findViewById<View>(R.id.button_detail_fragment).setOnClickListener {
             addFragment(DetailFragment())
 
@@ -86,7 +98,6 @@ class MainActivity : AppCompatActivity() {
             )
             toast.show()
         }
-
 
 
     }
